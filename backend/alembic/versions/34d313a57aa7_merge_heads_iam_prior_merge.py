72257111e95596658596cd91de4aa3bed6381066
Inneren12 <chulkov.oleksii.can@gmail.com>
2026-01-14 23:37:29 -0500

    fix: merge alembic heads for iam roles

---
+++ backend/alembic/versions/34d313a57aa7_merge_heads_iam_prior_merge.py
@@ -0,0 +1,19 @@
+"""merge heads (iam + prior merge)
+
+Revision ID: 34d313a57aa7
+Revises: 0085_iam_roles_permissions, 0086_merge_0085_heads
+Create Date: 2026-01-15 04:34:33.229942
+
+"""
+revision = "34d313a57aa7"
+down_revision = ("0085_iam_roles_permissions", "0086_merge_0085_heads")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
