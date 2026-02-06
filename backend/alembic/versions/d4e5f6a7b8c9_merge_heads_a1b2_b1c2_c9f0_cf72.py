c522619e94afbf1a4649860825ebf73e7e8ad6ce
Inneren12 <chulkov.oleksii.can@gmail.com>
2026-02-06 14:06:27 -0500

    Fix alembic merge down_revision

--- backend/alembic/versions/d4e5f6a7b8c9_merge_heads_a1b2_b1c2_c9f0_cf72.py
+++ backend/alembic/versions/d4e5f6a7b8c9_merge_heads_a1b2_b1c2_c9f0_cf72.py
@@ -1,13 +1,13 @@
 """merge heads a1b2c3d4e5f6 b1c2d3e4f5a6 c9f0a1b2c3d4 cf72c4eb59bc
 
 Revision ID: d4e5f6a7b8c9
-Revises: a1b2c3d4e5f6, b1c2d3e4f5a6, c9f0a1b2c3d4, cf72c4eb59bc
+Revises: b1c2d3e4f5a6, c9f0a1b2c3d4, cf72c4eb59bc
 Create Date: 2026-02-15 00:22:00.000000
 """
 from alembic import op
 
 revision = "d4e5f6a7b8c9"
-down_revision = ("a1b2c3d4e5f6", "b1c2d3e4f5a6", "c9f0a1b2c3d4", "cf72c4eb59bc")
+down_revision = ("b1c2d3e4f5a6", "c9f0a1b2c3d4", "cf72c4eb59bc")
 branch_labels = None
 depends_on = None
 
